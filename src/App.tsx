0e2ed44b4a7e2b5c6b518f560d02af8ac2d0f475
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-01 00:12:00 -0300

    chore: App setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyles } from "./styles/global";
+
 export function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
