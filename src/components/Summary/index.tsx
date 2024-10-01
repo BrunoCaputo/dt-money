1b88a62ccb730f70166a9d049a6cec2722bdb8b1
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-01 17:22:41 -0300

    feat: Transactions summary data

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,6 +9,25 @@ export function Summary() {
   const { transactions } = useContext(TransactionsContext);
   const theme = useTheme();
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -17,7 +36,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color={theme["green-300"]} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -26,7 +45,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color={theme["red-300"]} />
         </header>
 
-        <strong>R$ 1.259,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard $variant="green">
@@ -35,7 +54,7 @@ export function Summary() {
           <CurrencyDollar size={32} color={theme.white} />
         </header>
 
-        <strong>R$ 16.141,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
