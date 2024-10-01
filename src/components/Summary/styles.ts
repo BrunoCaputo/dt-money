deeef26ab2a82720613e61a292eed56d3125ad93
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-01 00:35:37 -0300

    feat: Transactions summary

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,46 @@
+import styled, { css } from "styled-components";
+
+const SummaryContainer = styled.section`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding-inline: 1.5rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+
+  margin-top: -5rem;
+`;
+
+interface SummaryCardProps {
+  $variant?: "green";
+}
+
+const SummaryCard = styled.div<SummaryCardProps>`
+  background: ${({ theme }) => theme["gray-600"]};
+  border-radius: 6px;
+  padding: 2rem;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: ${({ theme }) => theme["gray-300"]};
+  }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+  }
+
+  ${({ $variant, theme }) =>
+    $variant === "green" &&
+    css/*css*/ `
+      background: ${theme["green-700"]};
+    `
+  }
+`;
+
+export { SummaryCard, SummaryContainer };
