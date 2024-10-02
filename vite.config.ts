f29293f1cffdb201d0407999db425026bd21d251
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-02 18:19:38 -0300

    chore: Vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'build',
+    chunkSizeWarningLimit: 5000,
+  },
   plugins: [react()],
 })
